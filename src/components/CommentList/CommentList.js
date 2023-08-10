478f9cba0fb8d5392d1bca7a2b82d932eec96b2b
Haneul Roh <haneul.sky.roh@gmail.com>
2023-08-10 11:38:40 -0400

    Added backend part, only MainVideoDetail left

--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,45 +1,15 @@
 import CommentForm from "../CommentForm/CommentForm";
 import Comment from "../Comment/Comment"
 import "./CommentList.scss"
-import {useState, useEffect} from "react";
-import {useNavigate} from 'react-router-dom'
-import axios from "axios";
 
-const CommentList = ({videoID}) => { // seperate it later
-    const URL = "https://project-2-api.herokuapp.com/videos";
-    const KEY = "cf3d9906-0cb7-43ff-9c44-fea0189bb421";
-    const showFullURL = `${URL}/${videoID}?api_key=${KEY}`;
-
-    const navigate = useNavigate();
-    const [currentComments, setComments] = useState(null);
-
-
-    useEffect(() => {
-        axios.get(showFullURL).then((res) => {
-            const commentData = res.data;
-            setComments(commentData);
-        }).catch(error => {
-            console.log(error);
-            navigate('/');
-        })
-    }, [videoID])
-
-    if (currentComments === null) {
-        return (
-            <div className="loading__container">
-                <h2 className="loading"></h2>
-            </div>
-
-        )
-    }
-
-    // https://project-2-api.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=cf3d9906-0cb7-43ff-9c44-fea0189bb421
-
+const CommentList = ({currentVideo}) => { // seperate it later
 
     return (
-        <> 
-            <CommentForm currentVideo={currentComments}/> {
-            currentComments.comments.map((video) => {
+        <>
+            <CommentForm currentVideo={currentVideo}/> {
+        }
+            {
+            currentVideo.comments.map((video) => {
                 return <Comment key={
                         video.id
                     }
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,45 +7,49 @@ import MainVideoDetail from "../../components/MainVideoDetail/MainVideoDetail"
 import CommentList from "../../components/CommentList/CommentList"
 import VideoList from "../../components/VideoList/VideoList"
 
-
-
 const Main = () => {
-    
-    const URL = "https://project-2-api.herokuapp.com/videos";
-    const KEY = "cf3d9906-0cb7-43ff-9c44-fea0189bb421";
-    const showFullURL = `${URL}?api_key=${KEY}`;
 
     const {id} = useParams();
     const navigate = useNavigate();
-    const [video, setVideo] = useState(null);
+    const [videos, setVideos] = useState(null);
+    const [videoID, setVideoID] = useState("")
+    const [currentVideo, setcurrentVideo] = useState(null)
 
     useEffect(() => {
-        axios.get(showFullURL).then((res) => {
+        axios
+        .get("http://localhost:8080/videos").then((res) => {
             const videoData = res.data;
-            setVideo(videoData);
-        }).catch(error => {
+            setVideos(videoData);
+            const videoId = id || videoData[0].id;
+            setVideoID(videoId)
+            return axios.get(`http://localhost:8080/videos/${videoId}`)
+        })
+        .then((res) => {
+            const videoDetail = res.data;
+            setcurrentVideo(videoDetail)
+        })
+        .catch(error => {
             console.log(error);
             navigate('/');
         })
     }, [id])
 
-    if (video === null) {
+    if (currentVideo === null) {
         return <h3>...</h3>
     }
-    const videoID = id || video[0].id;
 
     return (
         <main className="main">
-            <MainVideo currentVideo={video}
+            <MainVideo currentVideo={videos}
                 videoID={videoID}/>
             <div className="main__container">
                 <div className="main__desktop">
-                    <MainVideoDetail currentVideo ={video}
+                    <MainVideoDetail currentVideo ={currentVideo}
                         videoID={videoID}/>
-                    <CommentList currentVideo ={video}
+                    <CommentList currentVideo ={currentVideo}
                         videoID={videoID}/>
                 </div>
-                <VideoList currentVideo={video}
+                <VideoList currentVideo={videos}
                     videoID={videoID}/>
             </div>
         </main>
@@ -53,4 +57,3 @@ const Main = () => {
 }
 
 export default Main;
-
